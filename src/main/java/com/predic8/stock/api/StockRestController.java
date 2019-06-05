9817212a150c2bdd38db3013ed711ba5eddf2031
Andreas Maier <a.maier@amitc.de>
2019-06-05 17:28:00 +0200

    third day commit

--- src/main/java/com/predic8/stock/api/StockRestController.java
+++ src/main/java/com/predic8/stock/api/StockRestController.java
@@ -4,6 +4,8 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.predic8.stock.model.Stock;
 import com.predic8.stock.event.Operation;
 import java.util.Collection;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.*;
 
@@ -33,5 +35,20 @@ public class StockRestController {
 	@GetMapping("/count")
 	public long count() {
 		return stocks.size();
+	}
+
+	@GetMapping("/{id}")
+	public Stock getStock(@PathVariable String id) {
+		return stocks.get(id);
+	}
+
+	@PutMapping
+	public void setStock(@RequestBody Stock stock) {
+		Stock availableStock = stocks.get(stock.getUuid());
+		if(availableStock != null) {
+			//stock.setUuid(availableStock.getUuid());
+			Operation op = new Operation("article", "upsert", mapper.valueToTree(stock));
+			kafka.send(new ProducerRecord<>("shop", op));
+		}
 	}
 }
